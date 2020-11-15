de1da1dce4350b6b02cc96cfd760a54ece6df66a
zahlulapaaja <zahlul.2001@gmail.com>
2020-11-15 21:05:24 +0700

    tes

--- app/Views/templates/template.php
+++ app/Views/templates/template.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Test</title>
+    <title>Ini adalah folder tes</title>
     <link rel="stylesheet" href="http://localhost/ngobar/public/css/output.css">
     <!-- <link href="http://localhost/ngobar/public/css/output.css"> -->
 </head>

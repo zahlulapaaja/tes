92fad0ccc1e2ed474e3951b200e40eb6d291aafe
zahlulapaaja <zahlul.2001@gmail.com>
2020-11-16 16:51:02 +0700

    ganti judul dan hapus file welcome.php

--- app/Views/templates/template.php
+++ app/Views/templates/template.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ini adalah folder tes</title>
+    <title>Halaman home</title>
     <link rel="stylesheet" href="http://localhost/ngobar/public/css/output.css">
     <!-- <link href="http://localhost/ngobar/public/css/output.css"> -->
 </head>
